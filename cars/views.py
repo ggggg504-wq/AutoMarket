5d9698203c77250724f31afab61130759bdfb043
dinoo <ggggg504@mail.ru>
2026-08-28 03:16:28 +0500

    feat: add pagination with filter persistence

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .filters import CarsFilter
 from .models import Car
-
+from django.core.paginator import Paginator
 
 def car(request, car_id):
     car = get_object_or_404(Car, id=car_id)
@@ -10,4 +10,10 @@ def car(request, car_id):
 
 def cars(request):
     qs_filter = CarsFilter(request.GET, queryset=Car.objects.all())
-    return render(request, 'cars.html', {'filter': qs_filter})
+    car_list = qs_filter.qs
+    paginator = Paginator(car_list, 1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    params = request.GET.copy()
+    params.pop('page', None)
+    return render(request, 'cars.html', {'filter': qs_filter, 'params': params.urlencode(),'page_obj': page_obj})
