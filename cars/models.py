e284712e72beeee806afb23b192aa8b84af0c783
dinoo <ggggg504@mail.ru>
2026-09-22 03:02:45 +0500

    Prepare project for deployment

--- cars/models.py
+++ cars/models.py
@@ -50,7 +50,7 @@ class Car(models.Model):
     mileage = models.PositiveIntegerField()
     city = models.CharField(max_length=30, choices=CITY_CHOICES)
     description = models.TextField(blank=True)
-    engine = models.DecimalField(max_digits=4, decimal_places=2)
+    engine = models.DecimalField(max_digits=3, decimal_places=1)
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
 
